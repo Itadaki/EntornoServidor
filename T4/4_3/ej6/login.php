a4f7b8a7e3ead346c0ce42806fc3d607a63da64b
Diego <parca_11@yahoo.es>
2015-01-23 14:05:38 +0100

    CREADA seccion AJAX de Entorno Cliente
    Añadido una nueva carpeta para todos los ejemplos y ejercicios del nuevo
    tema de AJAX de entorno cliente (necesitamos interaccion con servidor)

---
+++ T4/4_3/ej6/login.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+include ("funciones.php");
+include ("carrodelacompra.php");
+include("constantes.php");
+if (isset($_POST["login"])) {
+    login();
+} elseif (isset($_GET["action"]) and $_GET["action"] == "logout") {
+    logout();
+} elseif (isset($_SESSION["username"])) {
+    if (!isset($_SESSION["carro"]))
+        $_SESSION["carro"] = array();
+    if (isset($_GET["action"]) and $_GET["action"] == "addItem") {
+        addItem();
+    } elseif (isset($_GET["action"]) and $_GET["action"] == "removeItem") {
+        removeItem();
+    } elseif (isset($_GET["action"]) and $_GET["action"] == "carro") {
+        displayCarro();
+    } else {
+        displayProductos();
+    }
+} else {
+    displayForm();
+}
